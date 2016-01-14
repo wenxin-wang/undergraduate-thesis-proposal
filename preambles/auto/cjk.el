f5651e4e37436a4fbdec9c57b79c1d56203f5e1d
Wenxin Wang <wenxin.wang94@gmail.com>
2016-01-14 16:44:04 +0800

    initial commit

---
+++ preambles/auto/cjk.el
@@ -0,0 +1,6 @@
+(TeX-add-style-hook
+ "cjk"
+ (lambda ()
+   (TeX-run-style-hooks
+    "xeCJK")))
+
---
+++ preambles/auto/packages.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "packages"
+ (lambda ()
+   (TeX-run-style-hooks
+    "times"
+    "graphicx")))
+
